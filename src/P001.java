7193c3637da5a2960c0d633c4860e0049bc636c3
Seonwoo <primenumber_@naver.com>
2022-08-11 00:13:05 +0900

    1. 문제 001 숫자의 합 구하기 문제 풀이 (주석 추가 및 일부 변수명 변경)

--- src/P001.java
+++ src/P001.java
@@ -4,16 +4,20 @@ public class P001 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // 입력할 숫자의 개수
         int n = scanner.nextInt();
 
+        // 숫자 n개를 공백 없이 입력받는 변수
         String s = scanner.next();
 
-        String[] charArr = s.split("");
+        // 공백 없이 입력받은 숫자를 하나씩 쪼개어 변수형태로 넣어줌
+        String[] sArr = s.split("");
 
+        // 각 숫자의 합을 저장할 변수
         int sum = 0;
 
-        for (int i = 0; i < charArr.length; i++) {
-            sum += Integer.parseInt(charArr[i]);
+        for (int i = 0; i < sArr.length; i++) {
+            sum += Integer.parseInt(sArr[i]);
         }
 
         System.out.println(sum);
