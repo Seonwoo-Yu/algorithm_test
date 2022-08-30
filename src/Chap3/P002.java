2ef3511680e683c444bae92084c4788bfa3ba7a7
Seonwoo <primenumber_@naver.com>
2022-08-30 23:40:35 +0900

    Create package 'Chap3' and solve P002

--- src/Chap3/P002.java
+++ src/Chap3/P002.java
@@ -1,2 +1,35 @@
-package Chap3;public class P002 {
+package Chap3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P002 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        double max = arr[arr.length - 1];
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i] / max * 100;
+        }
+
+        double avg = sum / n;
+
+        System.out.println(avg);
+
+        scanner.close();
+
+    }
+
 }
